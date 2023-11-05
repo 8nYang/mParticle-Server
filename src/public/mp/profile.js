1b76265ba479411d84fd840398310ba573c4c5e6
8nYang <hsyang@mparticle.com>
2023-11-05 15:54:56 +0000

    Update APIKey for Profile API

--- src/public/mp/profile.js
+++ src/public/mp/profile.js
@@ -11,8 +11,10 @@ async function request_token() {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            client_id: "wawVx2Ejl0K2FUo79pyr6CQWyRmEuXSE",
-            client_secret: "zK57_GIo6de0UHThW7iVEac3d6od4pRVDD0GbAcuaEpRZtlBTeXw0bxYf4KIhhvA",
+            //client_id: "wawVx2Ejl0K2FUo79pyr6CQWyRmEuXSE", //us2 mParticle Workshops
+            //client_secret: "zK57_GIo6de0UHThW7iVEac3d6od4pRVDD0GbAcuaEpRZtlBTeXw0bxYf4KIhhvA", //us2 mParticle Workshops
+            client_id: "irrAuwex0d7calyRaknpCYE89omlKEDU", //eu1 mParticle Workshops
+            client_secret: "tMZaugEg57IyInCMiySuSU0c4UvB60s4C4ir5FH2tjzYScuUMcWaldjmE1rt9Lnn", //eu1 mParticle Workshops
             audience: "https://api.mparticle.com",
             grant_type: "client_credentials"
         }),
@@ -26,7 +28,8 @@ async function request_token() {
 
 async function request_user_profile(wSID, mPID) {
     s_token = await request_token();
-    var t = "https://api.mparticle.com/userprofile/v1/5000011/167/" + wSID + "/" + mPID + "?fields=user_identities,user_attributes,audience_memberships,attribution";
+    //var t = "https://api.mparticle.com/userprofile/v1/5000011/167/" + wSID + "/" + mPID + "?fields=user_identities,user_attributes,audience_memberships,attribution"; //us2 mParticle Workshops
+    var t = "https://api.mparticle.com/userprofile/v1/4000268/374/" + wSID + "/" + mPID + "?fields=user_identities,user_attributes,audience_memberships,attribution"; // eu1 mParticle Workshops
     var e = {
         headers: {
             Authorization: "Bearer " + s_token
